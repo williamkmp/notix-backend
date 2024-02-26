b7dcaf5c0748ccbab12f3dfdae0b6bd4e344b2aa
williamkmp <williamkurnia123@gmail.com>
2024-02-26 22:25:43 +0700

    feat: add FileDto

---
+++ src/main/java/com/william/notix/dto/FileDto.java
@@ -0,0 +1,16 @@
+package com.william.notix.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+public class FileDto {
+    private String id;
+    private String name;
+    private Long size;
+    private String url;
+    private String contentType;
+}
