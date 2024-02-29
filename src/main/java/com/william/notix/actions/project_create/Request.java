31081c843db01cee6720925283e3e61ab20d38cf
williamkmp <williamkurnia123@gmail.com>
2024-02-29 17:00:26 +0700

    feat: remove active period validation guard whne creating a project

--- src/main/java/com/william/notix/actions/project_create/Request.java
+++ src/main/java/com/william/notix/actions/project_create/Request.java
@@ -1,11 +1,12 @@
 package com.william.notix.actions.project_create;
 
+import java.util.Date;
+
 import com.william.notix.utils.values.VALIDATION;
-import jakarta.validation.constraints.FutureOrPresent;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.util.Date;
 import lombok.Value;
 
 @Value
@@ -15,9 +16,9 @@ public class Request {
     @Size(max = 255, message = VALIDATION.NUMBER_MAX + 255)
     private String name;
 
-    @NotNull(message = VALIDATION.REQUIRED) @FutureOrPresent(message = VALIDATION.DTM_FUTURE)
+    @NotNull(message = VALIDATION.REQUIRED)
     private Date startDate;
 
-    @NotNull(message = VALIDATION.REQUIRED) @FutureOrPresent(message = VALIDATION.DTM_FUTURE)
+    @NotNull(message = VALIDATION.REQUIRED)
     private Date endDate;
 }
