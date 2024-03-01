55ab52ae24706f826c65efa189ce1aa37dea9022
williamkmp <williamkurnia123@gmail.com>
2024-03-02 00:43:55 +0700

    feat: add owner user role checking

--- src/main/java/com/william/notix/services/AuthorityService.java
+++ src/main/java/com/william/notix/services/AuthorityService.java
@@ -1,5 +1,6 @@
 package com.william.notix.services;
 
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.william.notix.entities.Authority;
 import com.william.notix.entities.Project;
 import com.william.notix.repositories.AuthorityRepository;
+import com.william.notix.repositories.ProjectRepository;
 import com.william.notix.utils.values.ROLE;
 
 import lombok.AllArgsConstructor;
@@ -17,6 +19,7 @@ import lombok.NonNull;
 public class AuthorityService {
 
     private final AuthorityRepository authorityRepository;
+    private final ProjectRepository projectRepository;
 
     /**
      * get the user ROLE in a given project, else return empty if user have no access
@@ -30,6 +33,12 @@ public class AuthorityService {
         @NonNull Long projectId
     ) {
         try {
+            Project project = projectRepository.findById(projectId)
+                .orElseThrow(Exception::new);
+            Long ownerId = project.getOwner().getId();
+            if(Objects.equals(userId, ownerId)) {
+                return Optional.of(ROLE.PROJECT_MANAGER);
+            }
             Authority authority = authorityRepository
                 .findByUserAndProject(userId, projectId)
                 .orElseThrow(Exception::new);
