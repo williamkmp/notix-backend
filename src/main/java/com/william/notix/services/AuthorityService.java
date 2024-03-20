06d1d0c5cee2516ed8a36ecd49468bd03b55bf8c
williamkmp <williamkurnia123@gmail.com>
2024-03-20 17:18:34 +0700

    feat: add updateMemberRole() and role checking

--- src/main/java/com/william/notix/services/AuthorityService.java
+++ src/main/java/com/william/notix/services/AuthorityService.java
@@ -5,6 +5,7 @@ import com.william.notix.entities.Project;
 import com.william.notix.repositories.AuthorityRepository;
 import com.william.notix.repositories.ProjectRepository;
 import com.william.notix.utils.values.ROLE;
+import jakarta.transaction.Transactional;
 import java.util.Objects;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
@@ -22,7 +23,7 @@ public class AuthorityService {
      * get the user ROLE in a given project, else return empty if user have no access
      *
      * @param userId {@link Long} user id
-     * @param projectId {@link Project} project id
+     * @param projectId {@link Long} project id
      * @return {@link Optional}<{@link ROLE}> user's role, else empty
      */
     public Optional<ROLE> getUserProjectRole(
@@ -44,5 +45,61 @@ public class AuthorityService {
         } catch (Exception e) {
             return Optional.empty();
         }
+    }
+
+    /**
+     * update a member role in a given project
+     *
+     * @param userId {@link Long} user id
+     * @param projectId {@link Long} project id
+     * @param newRole {@link ROLE} new role
+     * @return {@link Optional}<{@link ROLE}> new role, else empty if fails
+     */
+    @Transactional
+    public Optional<ROLE> updateMemberRole(
+        @NonNull Long userId,
+        @NonNull Long projectId,
+        @NonNull ROLE newRole
+    ) {
+        try {
+            Project project = projectRepository
+                .findById(projectId)
+                .orElseThrow(Exception::new);
+            Long ownerId = project.getOwner().getId();
+            if (Objects.equals(userId, ownerId)) {
+                return Optional.of(ROLE.PROJECT_MANAGER);
+            }
+            Authority authority = authorityRepository
+                .findByUserAndProject(userId, projectId)
+                .orElseThrow(Exception::new);
+            authority.setRole(newRole);
+            authority = authorityRepository.save(authority);
+            return Optional.of(authority.getRole());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * check if a given role can do operation to a project member's
+     * i.e. ADD, DELETE, UPDATE.
+     *
+     * @param role {@link ROLE}
+     * @return {@link boolean} role can operate project member
+     */
+    public boolean roleCanOperateMember(@NonNull ROLE role) {
+        return Objects.equals(role, ROLE.PROJECT_MANAGER);
+    }
+
+    /**
+     * check if a given role can do operation to a project data
+     * , UPDATE project data i.e. name, active-period, etc.
+     *
+     * @param role {@link ROLE}
+     * @return {@link boolean} role can operate project member
+     */
+    public boolean roleCanOperateProject(@NonNull ROLE role) {
+        return Objects.equals(role, ROLE.PROJECT_MANAGER);
     }
 }
