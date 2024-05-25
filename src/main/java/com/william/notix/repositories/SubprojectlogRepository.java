50415d43f1dc86ada4c5c64b72c4c1cda86e2b5f
williamkmp <williamkurnia123@gmail.com>
2024-05-25 17:37:32 +0700

    feat: add SubprojectLog Repository

---
+++ src/main/java/com/william/notix/repositories/SubprojectlogRepository.java
@@ -0,0 +1,27 @@
+package com.william.notix.repositories;
+
+import com.william.notix.entities.ProjectLog;
+import com.william.notix.entities.SubprojectLog;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubprojectlogRepository
+    extends JpaRepository<SubprojectLog, Long> {
+    @Query(
+        """
+        SELECT log
+        FROM  suproject_logs log
+        WHERE log.updatee.id = :subprojectId
+        ORDER BY createdAt DESC
+        """
+    )
+    public List<ProjectLog> findAllById(
+        @Param("subprojectId") Long subprojectId,
+        Pageable page
+    );
+}
